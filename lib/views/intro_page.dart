4c0b76bbedf74b5a410800fd332bf1b315867902
Enzo Meschesi <enzomeschesi@gmail.com>
2024-06-13 17:50:51 -0300

    intro page update

--- lib/views/intro_page.dart
+++ lib/views/intro_page.dart
@@ -2,6 +2,7 @@ import 'package:ddnuvem/services/local_storage/booleans.dart';
 import 'package:ddnuvem/services/local_storage/local_storage_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../views/login_page.dart';
 
 class IntroPage extends StatefulWidget {
   const IntroPage({super.key});
@@ -37,8 +38,42 @@ class _IntroPageState extends State<IntroPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      child: Text("TODO: IntroPage"),
+    return Scaffold(
+        body: Center(
+          child: Row(
+            children: [
+              Container(
+                child: Image(image: AssetImage('assets/direto_da_nuvem_icon.png')),
+              ),
+              Container(
+                child: Text("Bem vindo ao Direto da Nuvem."),
+              ),
+              Container(
+                child: Text("Para utilizar os nossos serviços é necessária uma conta institucional iduff "),
+              ),
+              Container(
+                child: Text(" Após o Login: "),
+              ),
+              Container(
+                child: Text(" Certifique-se que a sua conta é instaladora "),
+              ),
+              Container(
+                child: Text(" Escreva o nome da televisão na qual está sendo instalado, e a adicione a um grupo já existente "),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()),
+                  );
+                },
+                child: Text("Vamos começar."),
+              ),
+
+            ],
+          ),
+        )
     );
+
   }
 }
