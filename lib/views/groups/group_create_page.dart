9baec259bbc1fd170abbe784c436b8fac331c9a4
Enzo Meschesi <enzomeschesi@gmail.com>
2024-06-21 23:03:52 -0300

    save

--- lib/views/groups/group_create_page.dart
+++ lib/views/groups/group_create_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ddnuvem/services/direto_da_nuvem/direto_da_nuvem_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -36,6 +37,8 @@ class _GroupCreatePageState extends State<GroupCreatePage> {
   DateTime? updatedAt;
   List<String>? admins;
 
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,7 +72,9 @@ class _GroupCreatePageState extends State<GroupCreatePage> {
                   admins = value as List<String>?;
                 }
             ),
-            DropdownButton(items: List.from(Firestore.collection('queues').where('name' isEqualTo: name).get()), onChanged: currentQueue = this.queues),
+            DropdownButton(items: List.from(
+
+                FirebaseFirestore.collection('queues').where('name' isEqualTo: name).get()), onChanged: currentQueue = this.queues),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: ElevatedButton(
