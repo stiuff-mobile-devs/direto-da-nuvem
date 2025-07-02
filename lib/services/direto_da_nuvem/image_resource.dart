ed04a187a2a784aceaa00b4de848e03894aec043
Carina Lanchine <92098147+carinalanchine@users.noreply.github.com>
2025-07-01 22:34:18 -0300

    feat: image data saved locally

--- lib/services/direto_da_nuvem/image_resource.dart
+++ lib/services/direto_da_nuvem/image_resource.dart
@@ -2,14 +2,39 @@ import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ddnuvem/models/image.dart';
+import 'package:ddnuvem/models/image_ui.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:hive/hive.dart';
 
 class ImageResource {
+  static const collection = "images";
   final FirebaseStorage _storage = FirebaseStorage.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final Box<ImageUI> _box = Hive.box<ImageUI>(collection);
 
   Future<Uint8List?> fetchImageData(String url) async {
-    return await _storage.ref().child(url).getData();
+    try {
+      final image = _box.values.firstWhere((image) => image.path == url);
+      return image.data;
+    } catch (e) {
+      return await _fetchImageFromStorage(url);
+    }
+  }
+
+  Future<Uint8List?> _fetchImageFromStorage(String url) async {
+    Uint8List? imageData = await _storage.ref().child(url).getData();
+    if (imageData != null) {
+      ImageUI newImage = ImageUI(
+        path: url,
+        data: imageData,
+        loading: false,
+        uploaded: true,
+      );
+
+      _box.put(newImage.path, newImage);
+      return imageData;
+    }
+    return null;
   }
 
   Future<String> uploadImage(String path, Uint8List data) async {
---
+++ lib/utils/connection_utils.dart
@@ -0,0 +1,21 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:http/http.dart' as http;
+
+Future<bool> hasInternetConnection() async {
+  var connectivity = await Connectivity().checkConnectivity();
+  if (connectivity == ConnectivityResult.none) {
+    return false;
+  }
+
+  try {
+    final result = await http.get(Uri.parse('https://www.google.com'))
+        .timeout(const Duration(seconds: 5));
+    if (result.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (_) {
+    return false;
+  }
+}
