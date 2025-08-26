c9ca9aed854bbcac178c6c380bd9dc1313a87470
João Vítor <jvlgoncalves@id.uff.br>
2025-08-26 18:16:11 -0300

    refactor: tentei deixar o código mais modular, para melhorar a legibilidade e eventual manutenção

---
+++ lib/views/people/active_filter_badges_widget.dart
@@ -0,0 +1,25 @@
+import 'package:ddnuvem/views/people/people_filter_controller.dart';
+import 'package:ddnuvem/views/people/privilege_filter_badge.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ActiveFilterBadgesWidget extends StatelessWidget {
+  const ActiveFilterBadgesWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(child: Consumer<PeopleFilterController>(
+      builder: (context, value, child) {
+      return Wrap(
+        alignment: WrapAlignment.center,
+        children: value.filters
+          .map((e) => PrivilegeFilterBadge(
+            filter: e,
+            onAdd: value.addFilter,
+            onRemove: value.removeFilter))
+          .toList());
+        }
+      )
+    );
+  }
+}
---
+++ lib/views/people/people_app_bar_widget.dart
@@ -0,0 +1,29 @@
+import 'package:ddnuvem/views/people/people_filter_drawer.dart';
+import 'package:ddnuvem/views/people/search_bar_widget.dart';
+import 'package:flutter/material.dart';
+
+class PeopleAppBarWidget extends StatelessWidget {
+  const PeopleAppBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      title: const Text("Pessoas"),
+      actions: [
+        IconButton(
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return const PeopleFilterDrawer();
+              });
+          },
+          icon: const Icon(Icons.filter_list))
+      ],
+      bottom: const PreferredSize(
+        preferredSize: Size.fromHeight(56),
+        child: PeopleSearchBar()
+      )
+    );
+  }
+}
